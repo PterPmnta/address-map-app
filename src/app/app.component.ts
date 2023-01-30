a741063f8e397e421f5e2bc1b66bd3f862d01483
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-01-29 19:40:47 -0500

    mapbox token applied global config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from "mapbox-gl";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'address-map-app';
+
+  ngOnInit(): void {
+    (mapboxgl as any).accessToken = 'pk.eyJ1IjoicHRlcnBtbnRhbSIsImEiOiJjbDU1enVzY2Qwb2FuM2txZnh4dTBrcWJ1In0.W2ustO6xfGcz-kjaLp4Udw'
+  }
 }
