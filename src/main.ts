2f05bdc2f1502776a03681ece0854e51c0740bd5
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-01-29 19:40:26 -0500

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-if(!navigator.geolocation){
+if (!navigator.geolocation) {
   alert('El navegador no soporta geocalizacion')
   throw new Error('Navegador no soporta geocalizacion');
 }
 
 import { AppModule } from './app/app.module';
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
