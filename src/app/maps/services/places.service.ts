ee873e7a90db204fd9664875d81f4ed161158d3a
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-01-30 16:57:33 -0500

    new attribute, function refactor and other things

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, IPlacesResponse } from '../interfaces/places.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +8,14 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   public userLocation : [number, number] | undefined;
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
 
   get isUserLocationReady(): boolean {
     return !!this.userLocation;
   }
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.getUserLocation();
   }
 
@@ -29,5 +33,18 @@ export class PlacesService {
         }
       )
     })
+  }
+
+  getPlacesByQuery(query:string = ''){
+
+    this.isLoadingPlaces = true;
+
+    this.http.get<IPlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=4&proximity=-73.990593, 40.740121&language=es&access_token=pk.eyJ1IjoicHRlcnBtbnRhbSIsImEiOiJjbDU1enVzY2Qwb2FuM2txZnh4dTBrcWJ1In0.W2ustO6xfGcz-kjaLp4Udw`)
+      .subscribe((resp)=>{
+        console.log(resp.features);
+        this.isLoadingPlaces = false;
+        this.places = resp.features;
+      })
+
   }
 }
