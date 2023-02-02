16bede37f815d8830a84445ef404893ab9b40c84
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-02-01 19:21:30 -0500

    linestring drawed after click over direcciones opt

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -98,8 +98,46 @@ export class MapService {
     });
 
     this.map?.fitBounds(bounds, {
-      padding: 100
+      padding: 200
     });
+
+    const sourceData: mapboxgl.AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords
+            }
+          }
+        ]
+      }
+    }
+
+    if(this.map.getLayer('RouteString')){
+      this.map.removeLayer('RouteString');
+      this.map.removeSource('RouteString');
+    }
+
+    this.map?.addSource('RouteString', sourceData);
+
+    this.map.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        'line-cap': 'round',
+        'line-join': 'round'
+      },
+      paint: {
+        'line-color': 'black',
+        "line-width": 2,
+      }
+    })
   }
 
 }
