22d6d0f654e3e87e72a1cc3e43364c3370a68582
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-02-01 19:05:24 -0500

    fit bound using a linestring finished

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { Feature } from '../interfaces/places.interface';
+import { DirectionsApiClient } from '../api';
+import { IDirectionsResponse, Route } from '../interfaces/directions.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,7 @@ export class MapService {
     return !!this.map;
   }
 
-  constructor() { }
+  constructor(private directionsApi: DirectionsApiClient) { }
 
   setMap(map: mapboxgl.Map){
     this.map = map;
@@ -72,6 +74,32 @@ export class MapService {
       padding: 200
     });
 
+  }
+
+  getRouteBetweenPoints(start: [number, number], end: [number, number]){
+    this.directionsApi.get<IDirectionsResponse>(`/${start.join(',')};${end.join(',')}`)
+      .subscribe(resp => {
+        this.drawLinestring(resp.routes[0])
+      })
+  }
+
+  private drawLinestring(route: Route){
+    console.log({kms: route.distance / 1000, duration: route.duration / 60})
+
+    if(!this.map){
+      throw Error('El mapa no inicializado.')
+    }
+
+    const coords = route.geometry.coordinates;
+    const bounds = new mapboxgl.LngLatBounds();
+
+    coords.forEach(([lng, lat]) => {
+      bounds.extend([lng, lat])
+    });
+
+    this.map?.fitBounds(bounds, {
+      padding: 100
+    });
   }
 
 }
